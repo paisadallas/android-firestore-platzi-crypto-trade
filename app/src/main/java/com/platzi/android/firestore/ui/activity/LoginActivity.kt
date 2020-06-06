e812afc1385f20f2653fe36d4e3184a0b0605209
FredyReyes <fredy.reyes.a@gmail.com>
2020-06-06 16:27:23 -0500

    Revert "Revert "Add Firebase""
    This reverts commit 241d65618385e1607e7c403d2a843bd737688cce.

--- app/src/main/java/com/platzi/android/firestore/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/platzi/android/firestore/ui/activity/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
+import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -32,9 +33,9 @@ class LoginActivity : AppCompatActivity() {
 
     private val TAG = "LoginActivity"
 
-    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
-
     lateinit var firestoreService: FirestoreService
+    //Access module autentication
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,15 +43,13 @@ class LoginActivity : AppCompatActivity() {
         firestoreService = FirestoreService(FirebaseFirestore.getInstance())
     }
 
-
     fun onStartClicked(view: View) {
         view.isEnabled = false
         auth.signInAnonymously()
             .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
+                if (task.isSuccessful) {                                                                //Autentication Acept
                     val username = username.text.toString()
                     firestoreService.findUserById(username, object : Callback<User> {
-
                         override fun onSuccess(result: User?) {
                             if (result == null) {
                                 val user = User()
